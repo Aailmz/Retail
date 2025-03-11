feecddcf0a212d1fefd678c5bb38c24db3f5ff9d
Aailmz <mirzazubaridjunaid@gmail.com>
2025-03-11 11:25:04 +0700

    fix: change bearer to jwt token

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,14 +1,19 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request?.cookies?.jwt;
+        },
+      ]),
       ignoreExpiration: false,
-      secretOrKey: 'rahasia', // Gunakan variabel lingkungan untuk produksi
+      secretOrKey: 'rahasia',
     });
   }
 
