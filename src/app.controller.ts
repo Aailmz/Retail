29f7bfc0785310ee7696f7417f4871f95df173b1
Aailmz <mirzazubaridjunaid@gmail.com>
2025-03-11 10:15:02 +0700

    feat: render view

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request, Render } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Controller()
 export class AppController {
   @UseGuards(JwtAuthGuard)
   @Get('profile')
+  @Render('profile')
   getProfile(@Request() req) {
-    return req.user;
+    return { user: req.user };
   }
 }
