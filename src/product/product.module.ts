b3d57cc8de6e0637db385570315107609a3907f7
Aailmz <mirzazubaridjunaid@gmail.com>
2025-03-14 15:20:17 +0700

    feat: product module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductService } from './product.service';
+import { ProductController } from './product.controller';
+import { Product } from './entities/product.entity';
+import { CategoryModule } from '../category/category.module';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    CategoryModule,
+  ],
+  controllers: [ProductController],
+  providers: [ProductService],
+})
 export class ProductModule {}
