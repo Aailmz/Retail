cb3e84f8736902e8e3e2f7274c086aa368480a15
Aailmz <mirzazubaridjunaid@gmail.com>
2025-03-14 17:10:11 +0700

    feat: use guards for products (temporary)

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
