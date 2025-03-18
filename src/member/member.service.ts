6f1ea688544e3c0e3d1ffe67722680d93e8811ad
Aailmz <mirzazubaridjunaid@gmail.com>
2025-03-18 14:09:24 +0700

    feat: member service

--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Member } from './entities/member.entity';
+import { CreateMemberDto } from './dto/create-member.dto';
+import { UpdateMemberDto } from './dto/update-member.dto';
 
 @Injectable()
-export class MemberService {}
+export class MemberService {
+  constructor(
+    @InjectRepository(Member)
+    private memberRepository: Repository<Member>,
+  ) {}
+
+  async create(createMemberDto: CreateMemberDto): Promise<Member> {
+    const member = this.memberRepository.create(createMemberDto);
+    return this.memberRepository.save(member);
+  }
+
+  async findAll(): Promise<Member[]> {
+    return this.memberRepository.find();
+  }
+
+  async findOne(id: number): Promise<Member> {
+    const member = await this.memberRepository.findOne({
+      where: { id },
+    });
+    
+    if (!member) {
+      throw new NotFoundException(`Member with ID ${id} not found`);
+    }
+    
+    return member;
+  }
+
+  async update(id: number, updateMemberDto: UpdateMemberDto): Promise<Member> {
+    const member = await this.findOne(id);
+    this.memberRepository.merge(member, updateMemberDto);
+    return this.memberRepository.save(member);
+  }
+
+  async remove(id: number): Promise<void> {
+    const member = await this.findOne(id);
+    await this.memberRepository.remove(member);
+  }
+}
