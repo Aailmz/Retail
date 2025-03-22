fb588b763973aa92f7d7f0a88fb0fad15495bce3
Aailmz <mirzazubaridjunaid@gmail.com>
2025-03-22 12:14:38 +0700

    feat: promotion service

--- src/promotion/promotion.service.ts
+++ src/promotion/promotion.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Promotion } from './entities/promotion.entity';
+import { CreatePromotionDto } from './dto/create-promotion.dto';
+import { UpdatePromotionDto } from './dto/update-promotion.dto';
 
 @Injectable()
-export class PromotionService {}
+export class PromotionService {
+  constructor(
+    @InjectRepository(Promotion)
+    private promotionRepository: Repository<Promotion>,
+  ) {}
+
+  async create(createPromotionDto: CreatePromotionDto): Promise<Promotion> {
+    const promotion = this.promotionRepository.create(createPromotionDto);
+    return this.promotionRepository.save(promotion);
+  }
+
+  async findAll(): Promise<Promotion[]> {
+    return this.promotionRepository.find();
+  }
+
+  async findOne(id: number): Promise<Promotion> {
+    const promotion = await this.promotionRepository.findOne({
+      where: { id },
+    });
+    
+    if (!promotion) {
+      throw new NotFoundException(`Promotion with ID ${id} not found`);
+    }
+    
+    return promotion;
+  }
+
+  async update(id: number, updatePromotionDto: UpdatePromotionDto): Promise<Promotion> {
+    const promotion = await this.findOne(id);
+    this.promotionRepository.merge(promotion, updatePromotionDto);
+    return this.promotionRepository.save(promotion);
+  }
+
+  async remove(id: number): Promise<void> {
+    const promotion = await this.findOne(id);
+    await this.promotionRepository.remove(promotion);
+  }
+  
+  async calculateMarkup(discountRate: number): Promise<number> {
+    return (1 / (1 - discountRate)) - 1;
+  }
+}
