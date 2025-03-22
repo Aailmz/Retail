515b6d1afe0b53084a65416b10cfaf0cec540a9d
Aailmz <mirzazubaridjunaid@gmail.com>
2025-03-22 12:38:36 +0700

    feat: promotion module

--- src/promotion/promotion.module.ts
+++ src/promotion/promotion.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PromotionService } from './promotion.service';
 import { PromotionController } from './promotion.controller';
+import { Promotion } from './entities/promotion.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Promotion])],
+  controllers: [PromotionController],
   providers: [PromotionService],
-  controllers: [PromotionController]
+  exports: [PromotionService],
 })
 export class PromotionModule {}
