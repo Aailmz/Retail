2e966db6767edb4df9cb12c98aa6da5ba19f0df2
Aailmz <mirzazubaridjunaid@gmail.com>
2025-04-05 18:55:44 +0700

    feat: transaction module temporary

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
+import { Transaction } from './entities/transaction.entity';
+import { TransactionItem } from './entities/transaction-item.entity';
+import { ProductModule } from '../product/product.module';
+import { PromotionModule } from '../promotion/promotion.module';
+import { Product } from '../product/entities/product.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Transaction, TransactionItem, Product]),
+    ProductModule,
+    PromotionModule
+  ],
+  controllers: [TransactionController],
   providers: [TransactionService],
-  controllers: [TransactionController]
+  exports: [TransactionService]
 })
 export class TransactionModule {}
+
+// utils/code-generator.ts
+export function generateTransactionCode(): string {
+  const prefix = 'TRX';
+  const timestamp = Date.now().toString().slice(-6);
+  const random = Math.floor(Math.random() * 1000).toString().padStart(3, '0');
+  return `${prefix}-${timestamp}${random}`;
+}
+
+// update-transaction.dto.ts
+import { IsOptional, IsEnum } from 'class-validator';
+import { PaymentStatus } from '../entities/transaction.entity';
+
+export class UpdateTransactionDto {
+  @IsOptional()
+  @IsEnum(PaymentStatus)
+  paymentStatus?: PaymentStatus;
+}
